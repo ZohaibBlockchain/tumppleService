ae5b260215d99d01f0de644d33c9906e4f06bb85
ZohaibBlockchain <113122909+ZohaibBlockchain@users.noreply.github.com>
2023-05-13 21:00:35 +0400

    v2

--- core.js
+++ core.js
@@ -1,12 +1,19 @@
 import express from 'express';
 const app = express();
 import cors from 'cors';
-const PORT = 2023;
+const PORT = 9999;
 import { retriveYTData } from './fx.js';
 import { detectService,UniversalService } from './uniFx.js';
 
 app.use(cors());
 app.use(express.json());
+
+
+const allowedOrigin = 'https://tumpple.com';
+
+
+
+
 
 // Route handler for POST requests to /welcome
 app.post('/api/v1/welcome', (req, res) => {
@@ -15,9 +22,10 @@ app.post('/api/v1/welcome', (req, res) => {
 
 
 app.post('/api/v1/youtubedata', async (req, res) => {
-  res.header('Access-Control-Allow-Origin', 'https://tumpple.com/'); // set the allowed origin
-  res.header('Access-Control-Allow-Methods', 'POST'); // set the allowed methods
-  res.header('Access-Control-Allow-Headers', 'Content-Type'); // set the allowed headers
+  // res.header('Access-Control-Allow-Origin', allowedOrigin); // set the allowed origin
+  // res.header('Access-Control-Allow-Methods', 'POST'); // set the allowed methods
+  // res.header('Access-Control-Allow-Headers', 'Content-Type'); // set the allowed headers
+  res.header('Access-Control-Allow-Origin', '*');
   let info = await retriveYTData(req.body.link);
   res.status(200).send(info);
 });
@@ -25,9 +33,10 @@ app.post('/api/v1/youtubedata', async (req, res) => {
 
 // Route handler for POST requests of universal
 app.post('/api/v1/uni', async (req, res) => {
-  // res.header('Access-Control-Allow-Origin', 'https://tumpple.com/'); // set the allowed origin
+  // res.header('Access-Control-Allow-Origin', allowedOrigin); // set the allowed origin
   // res.header('Access-Control-Allow-Methods', 'POST'); // set the allowed methods
   // res.header('Access-Control-Allow-Headers', 'Content-Type'); // set the allowed headers
+  res.header('Access-Control-Allow-Origin', '*');
   let service = detectService(req.body.link);
   if (service  == 'Unknown') {
     res.status(200).send({value:undefined});
@@ -41,7 +50,7 @@ app.post('/api/v1/uni', async (req, res) => {
 
 
 app.post('/api/v1/contact', async (req, res) => {
-  res.header('Access-Control-Allow-Origin', 'https://tumpple.com/'); // set the allowed origin
+  res.header('Access-Control-Allow-Origin', allowedOrigin); // set the allowed origin
   res.header('Access-Control-Allow-Methods', 'POST'); // set the allowed methods
   res.header('Access-Control-Allow-Headers', 'Content-Type'); // set the allowed headers
   res.status(200).send('Ack');
